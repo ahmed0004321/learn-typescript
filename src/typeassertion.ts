fc922216e1805b8d59a8792fa1ddb0bbab6d8030
Oasif Ahmed <oasif@Oasifs-Mac-mini.local>
2026-05-05 12:17:37 +0600

    type assertion

--- src/typeassertion.ts
+++ src/typeassertion.ts
@@ -17,7 +17,12 @@ string | number | undefined => {
 
 const result1 = kgtoGMConverter(23) as number;//function er return type 3 type thakleo
 //jodi amra sure jani je konta number and konta string hobe oitake
-// as string or as number likhe dilei done...
+// as string or as number likhe dilei done...akhon chaile amra otake die jekono type method use korte parbo
+//like result1.all the number methods
 console.log(result1);
 const result2 = kgtoGMConverter('2 KG') as string;
 console.log(result2);
+
+//final verdict:
+//typescript theke jokhon amra better type define korte parbo sheitai type assertion.
+//jokhon amra akdom e sure that ata must number or must string tokhon e as use kore type define koro.
